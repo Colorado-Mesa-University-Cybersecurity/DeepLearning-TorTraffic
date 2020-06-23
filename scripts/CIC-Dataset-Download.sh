2ca3433d5a9e4ba5467d4944aa0cddc2ad37c3e2
bkhadka2 <bishalkhadka1600@gmail.com>
2020-06-23 15:22:06 -0600

    script to download dataset

--- scripts/CIC-Dataset-Download.sh
+++ scripts/CIC-Dataset-Download.sh
@@ -2,21 +2,34 @@
 
 downloadFunction() {
 	mkdir ../CICDataSet-TOR
+	perl -e 'print "="x100; print "\n"'
 	echo "Data Set will be downloaded in the parent folder named CICDataSet-TOR"
-	perl -e 'print "="x50; print "\n"'
-	echo "Downloading Tor/ Non-Tor CIC Dataset........."
-	perl -e 'print "="x50; print "\n"'
+	perl -e 'print "="x100; print "\n"'
 	URL=http://205.174.165.80/CICDataset/ISCX-Tor-NonTor-2017/Dataset/
-	zipFile=TorCSV.zip
-	curl -o ../CICDataSet-TOR/$zipFile $URL$zipFile
-	echo "Unzipping the file ${zipFile}........."
+	zipFile1=TorCSV.zip
+	zipFile2=TorPcaps.zip
+	perl -e 'print "="x50; print "\n"'
+	echo "Downloading TorCSV CIC Dataset........."
+	perl -e 'print "="x50; print "\n"'
+	curl -o ../CICDataSet-TOR/$zipFile1 $URL$zipFile1
+	perl -e 'print "="x50; print "\n"'
+	echo "Downloading TorPcaps CIC Dataset........."
+	perl -e 'print "="x50; print "\n"'
+	curl -o ../CICDataSet-TOR/$zipFile2 $URL$zipFile2
+	perl -e 'print "="x50; print "\n"'
+	echo "Unzipping the file ${zipFile1}........."
 	perl -e 'print "="x50; print "\n"'
 	cd ../CICDataSet-TOR
-	unzip -o $zipFile
+	unzip -o $zipFile1
 	perl -e 'print "="x50; print "\n"'
-	echo "Deleting Zip file after extracting........"
+	echo "Unzipping the file ${zipFile2}........."
 	perl -e 'print "="x50; print "\n"'
-	rm $zipFile
+	unzip -o $zipFile2
+	perl -e 'print "="x50; print "\n"'
+	echo "Deleting Zip files after extracting........"
+	perl -e 'print "="x50; print "\n"'
+	rm $zipFile1
+	rm $zipFile2
 	perl -e 'print "="x50; print "\n"'
 	echo "Download and extraction complete.........."
 	perl -e 'print "="x50; print "\n"'
